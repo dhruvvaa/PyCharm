d3441d0243405200db8bbec51373cc9bed88e4e0
Dhruva Shah <dhruva.shah2506@gmail.com>
2026-01-08 14:32:35 +0530

    Add files via upload

---
+++ tupleFunctions.py
@@ -0,0 +1,24 @@
+pets = ('cats', 'dogs', 'fish')
+numbers = (5, 6, 2, 0, 4, 8)
+aliens = (0, False)
+
+#any()
+print(any(pets))
+print(any(numbers))
+print(any(aliens))
+
+#.count()
+print(numbers.count(5))
+print(numbers.count(6))
+
+#min()
+print(min(pets))
+print(min(numbers))
+
+#max
+print(max(numbers))
+
+#len()
+print(len(pets))
+print(len(aliens))
+
