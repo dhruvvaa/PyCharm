5caa03675d7534230c808e66b5bd3f632a5e3a14
Dhruva Shah <dhruva.shah2506@gmail.com>
2026-01-08 16:55:09 +0530

    Add files via upload

---
+++ setOperations.py
@@ -0,0 +1,23 @@
+a = {1,2,3}
+b = {3,4,5}
+
+#A union B
+print("a union b")
+print(a.union(b))
+print(a | b)
+
+#A intersection B
+print("a intersection b")
+print(a.intersection(b))
+print(a & b)
+
+#A equals to B
+print(a==b)
+
+#Subsets and Supersets
+print("Subsets and Supersets")
+print(a.issubset(b))
+print(b.issuperset(a))
+b.update(a)
+print(b)
+print(a.issubset(b))
