693a8a5912e0b6a65f0fec2d431cc58f3222dec3
Dhruva Shah <dhruva.shah2506@gmail.com>
2026-01-08 16:46:20 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,22 @@
+#create 2 sets
+a = {1,2,3}
+b = {3,4,5}
+
+#print elements
+print(a)
+
+#iterate through sets
+for item in a:
+    print(item)
+
+#add elements
+a.add(4)
+print(a)
+a.update(b)
+print(a)
+
+#remove items from sets in 3 different ways
+a.remove(5)
+a.discard(4)
+print(a.pop())
+print(a)
