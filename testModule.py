e5cb0d6b855c93745e56210b938540440a46f8c3
Dhruva Shah <dhruva.shah2506@gmail.com>
2026-01-10 17:47:55 +0530

    Add files via upload

---
+++ testModule.py
@@ -0,0 +1,5 @@
+import myModule
+
+r = float(input("Enter radius: "))
+print("The circumference of circle with radius",r,"is",myModule.circumference(r))
+print("The area of circle with radius",r,"is",myModule.area(r))
